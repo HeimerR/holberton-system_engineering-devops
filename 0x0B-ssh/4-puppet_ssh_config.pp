149cbb43ff15fa570099fe040ffd675b586ed5fe
heimerr <732@holbertonschool.com>
2019-08-06 17:22:12 +0000

    task 4

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+#make changes to our configuration file
+file_line { 'BatchMode yes':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'BatchMode yes',
+  match  => '^ BatchMode',
+}->
+file_line { 'IdentityFile':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/holberton',
+  match  => '^IdentityFile',
+}
