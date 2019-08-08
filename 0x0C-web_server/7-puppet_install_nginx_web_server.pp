2c952441d93d3b8d428ca37d2304124f9605afb8
HeimerR <ing.heimer.rojas@gmail.com>
2019-08-08 23:19:17 +0000

    task 7

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,2 +1,13 @@
 # Install Nginx web server
-class{'nginx': }
+package { 'nginx':
+	ensure => installed,
+}->
+file { '/var/www/html/index.nginx-debian.html':
+	content => 'Holberton School',
+}->
+file_line { 'redirect':
+	ensure => present,
+	path   => '/etc/nginx/sites-enabled/default',
+	line   => "listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/www.youtube.com\/;\\n\\t}",
+	match  => '^listen 80 default_server;',
+}
