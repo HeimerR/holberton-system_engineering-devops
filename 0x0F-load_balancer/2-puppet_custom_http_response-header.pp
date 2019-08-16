cab6ee8359f102b8af2099a7799479494858fbed
HeimerR <ing.heimer.rojas@gmail.com>
2019-08-16 19:04:50 +0000

    task 2

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -0,0 +1,17 @@
+# Install Nginx web server
+package {'nginx':
+  ensure => 'present',
+}->
+exec {'Holberton':
+  command => '/bin/echo "Holberton School" > /var/www/html/index.nginx-debian.html',
+  returns => [0,2]
+}->
+exec {'sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/www.youtube.com\/;\\n\\t}/" /etc/nginx/sites-enabled/default':
+  path => '/usr/bin:/usr/sbin:/bin',
+}->
+exec {'sed -i "s/http {/http {\\n\\tadd_header X-Served-By \"$HOSTNAME\";/" /etc/nginx/nginx.conf':
+  path => '/usr/bin:/usr/sbin:/bin',
+}->
+exec {'run':
+  command => '/usr/sbin/service nginx start',
+}
