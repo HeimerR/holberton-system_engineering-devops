4272fa10f1a2a530a99fa080e9ebb01f63b55e48
HeimerR <ing.heimer.rojas@gmail.com>
2019-08-16 20:42:22 +0000

    task 2 no installing

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,18 +1,13 @@
-# Install Nginx web server
-package {'nginx':
-  ensure => 'present',
-}->
-exec {'Holberton':
-  command => '/bin/echo "Holberton School" > /var/www/html/index.nginx-debian.html',
-  returns => [0,2]
-}->
-exec {'sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/www.youtube.com\/;\\n\\t}/" /etc/nginx/sites-enabled/default':
-  path => '/usr/bin:/usr/sbin:/bin',
-}->
-file_line { 'http_header':
-  path => '/etc/nginx/nginx.conf',
-  match => 'http {',
-  line => "http {\n\tadd_header X-Served-By \"${hostname}\";",
-}->
+# custom http header response NGiNX
 exec {'run':
+  command => '/usr/sbin/service nginx stop',
+}
+-> file_line { 'http_header':
+  path  => '/etc/nginx/nginx.conf',
+  match => 'http {',
+  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
+}
+-> exec {'run2':
   command => '/usr/sbin/service nginx start',
+  returns => [0,1,2]
+}
