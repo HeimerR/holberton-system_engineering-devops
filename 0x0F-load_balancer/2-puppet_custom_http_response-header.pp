bf30e8167f071545d95ad60d33cf99c572c16cb6
HeimerR <ing.heimer.rojas@gmail.com>
2019-08-16 20:55:09 +0000

    task 2 installin only nginx

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,6 +1,6 @@
 # custom http header response NGiNX
-exec {'run':
-  command => '/usr/sbin/service nginx stop',
+package {'nginx':
+  ensure => 'present',
 }
 -> file_line { 'http_header':
   path  => '/etc/nginx/nginx.conf',
@@ -9,5 +9,4 @@ exec {'run':
 }
 -> exec {'run2':
   command => '/usr/sbin/service nginx start',
-  returns => [0,1,2]
 }
