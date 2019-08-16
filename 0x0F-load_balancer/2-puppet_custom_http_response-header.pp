2b3c4ae987a84bd85831b96a7ef6b7ddb63cf57f
HeimerR <ing.heimer.rojas@gmail.com>
2019-08-16 20:05:54 +0000

    task 2 usong variable pupet line

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -9,9 +9,10 @@ exec {'Holberton':
 exec {'sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/www.youtube.com\/;\\n\\t}/" /etc/nginx/sites-enabled/default':
   path => '/usr/bin:/usr/sbin:/bin',
 }->
-exec {'sed -i "s/http {/http {\\n\\tadd_header X-Served-By \"$HOSTNAME\";/" /etc/nginx/nginx.conf':
-  path => '/usr/bin:/usr/sbin:/bin',
+file_line { 'http_header':
+  path => '/etc/nginx/nginx.conf',
+  match => 'http {',
+  line => "http {\n\tadd_header X-Served-By \"${hostname}\";",
 }->
 exec {'run':
   command => '/usr/sbin/service nginx start',
-}
