69a55685b1722f568b0583dd9ea533210cd60063
HeimerR <ing.heimer.rojas@gmail.com>
2019-09-16 21:39:36 +0000

    [fix] import order changed

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """ given employee ID, returns information about his/her TODO list progress """
-from sys import argv
 import requests
+from sys import argv
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     """ main code """
     params = {'userId': argv[1]}
     r_info = requests.get(
