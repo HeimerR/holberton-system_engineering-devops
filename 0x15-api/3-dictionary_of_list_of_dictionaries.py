a244d63527338849adb49cc8a00d70893ab5408c
HeimerR <ing.heimer.rojas@gmail.com>
2019-09-16 23:57:08 +0000

    task3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -4,26 +4,26 @@ import json
 import requests
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     """ main code """
     r_info = requests.get(
-        'https://jsonplaceholder.typicode.com/todos')
+        "https://jsonplaceholder.typicode.com/todos")
     r_users = requests.get(
-        'https://jsonplaceholder.typicode.com/users')
+        "https://jsonplaceholder.typicode.com/users")
     data = {}
     """ print(r_info.json()) """
     for user in r_users.json():
         todos = list(
             filter(
-                lambda i: i.get('userId') == user.get('id'),
+                lambda i: i.get("userId") == user.get("id"),
                 r_info.json()))
         list_dict = []
         for dictionary in todos:
             new_dict = {}
-            new_dict['task'] = dictionary['title']
-            new_dict['completed'] = dictionary['completed']
-            new_dict['username'] = user.get('username')
+            new_dict["task"] = dictionary["title"]
+            new_dict["completed"] = dictionary["completed"]
+            new_dict["username"] = user.get("username")
             list_dict.append(new_dict)
-        data[user.get('id')] = list_dict
+        data[user.get("id")] = list_dict
     with open("todo_all_employees.json", "w") as f:
         json.dump(data, f)
