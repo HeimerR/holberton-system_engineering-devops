e9681202d5d0068bcc6a84d1c034a0e99fd4d88e
HeimerR <ing.heimer.rojas@gmail.com>
2019-09-16 22:56:07 +0000

    [fix] colon added in the print

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
             lambda elem: elem.get('id') == int(
                 argv[1]),
             r_users))[0]
-    print('Employee {} is done with'.format(r_user_dict.get('name')), end='')
+    print('Employee {} is done with '.format(r_user_dict.get('name')), end='')
     r_info_filtered = list(
         filter(
             lambda elem: elem.get('userId') == int(
@@ -27,6 +27,6 @@ if __name__ == "__main__":
             r_info_filtered))
     NUMBER_OF_DONE_TASKS = len(DONE_TASKS)
     TOTAL_NUMBER_OF_TASKS = len(r_info_filtered)
-    print('tasks({}/{})'.format(NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
+    print('tasks({}/{}):'.format(NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
     for i in DONE_TASKS:
         print('\t {}'.format(i.get('title')))
