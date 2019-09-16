84c17eea34e887f595ccb945604b82e72e3271ff
HeimerR <ing.heimer.rojas@gmail.com>
2019-09-16 23:41:21 +0000

    task 2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -5,24 +5,24 @@ import requests
 from sys import argv
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     """ main code """
-    params = {'userId': argv[1]}
+    params = {"userId": argv[1]}
     r_info = requests.get(
-        'https://jsonplaceholder.typicode.com/todos',
+        "https://jsonplaceholder.typicode.com/todos",
         params=params)
     r_user = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1]))
+        "https://jsonplaceholder.typicode.com/users/{}".format(argv[1]))
     r_info_filtered = list(
         filter(
-            lambda elem: elem.get('userId') == int(
+            lambda elem: elem.get("userId") == int(
                 argv[1]),
             r_info.json()))
     for dictionary in r_info_filtered:
-        dictionary['task'] = dictionary.pop('title')
-        del dictionary['userId']
-        del dictionary['id']
-        dictionary['username'] = r_user.json().get('username')
+        dictionary["task"] = dictionary.pop("title")
+        del dictionary["userId"]
+        del dictionary["id"]
+        dictionary["username"] = r_user.json().get("username")
     data = {argv[1]: r_info_filtered}
     with open("{}.json".format(argv[1]), "w") as f:
         json.dump(data, f)
