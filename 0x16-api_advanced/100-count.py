04426c8b4a6442eedbbf361d247ab1ff6de8f868
HeimerR <ing.heimer.rojas@gmail.com>
2019-09-20 02:02:50 +0000

    [fix]duplicates added to total

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -18,8 +18,8 @@ def count_words(subreddit, word_list, hot_list=[], init=0, after="null"):
         if init == 0:
             hot_str = " ".join(hot_list)
             hot_words = hot_str.split(" ")
-            word_list_low = sorted(list(set(word_list)))
-            rst = []
+            word_list_low = sorted(word_list)
+            rt = []
             for word in word_list_low:
                 num = len(
                     list(
@@ -27,8 +27,17 @@ def count_words(subreddit, word_list, hot_list=[], init=0, after="null"):
                             lambda hot_w: hot_w.lower() == word.lower(),
                             hot_words)))
                 if num != 0:
-                    rst.append([word, num])
-            if len(rst) != 0:
-                r = sorted(rst, key=lambda x: (x[1]), reverse=True)
-                for i in r:
+                    rt.append([word, num])
+            if len(rt) != 0:
+                i = 0
+                while i < len(rt) - 1:
+                    if rt[i + 1][0] is not None and rt[i][0] == rt[i + 1][0]:
+                        rt[i][1] += rt[i + 1][1]
+                        rt.pop(i + 1)
+                        rt.append([None, None])
+                        i -= 1
+                    i += 1
+                r = list(filter(lambda x: x != [None, None], rt))
+                r_sorted = sorted(r, key=lambda x: (x[1]), reverse=True)
+                for i in r_sorted:
                     print(*i, sep=": ")
