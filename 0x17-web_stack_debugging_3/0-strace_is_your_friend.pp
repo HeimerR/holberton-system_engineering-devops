18942b1005b7dad3bb537e4a47fed34b4c661403
HeimerR <ing.heimer.rojas@gmail.com>
2019-10-02 13:57:13 +0000

    apt get update added to the manifest

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,8 @@
 # fixes corrupted file with extension phpp
-file_line { 'replace line':
+exec {'restart service':
+  command => '/usr/bin/apt-get update',
+}
+-> file_line { 'replace line':
   path  => '/var/www/html/wp-settings.php',
   match => 'require_once\( ABSPATH \. WPINC \. \'/class-wp-locale\.phpp\' \);',
   line  => 'require_once( ABSPATH . WPINC . \'/class-wp-locale.php\' );',
