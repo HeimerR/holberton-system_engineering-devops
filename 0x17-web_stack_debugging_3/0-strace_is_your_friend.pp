71f645b3e1d0188308a9fc690d97a789e9aba15c
HeimerR <ing.heimer.rojas@gmail.com>
2019-10-02 14:25:28 +0000

    using sed instead file_line

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,11 +1,6 @@
 # fixes corrupted file with extension phpp
-exec {'restart service':
-  command => '/usr/bin/apt-get update',
-}
--> file_line { 'replace line':
-  path  => '/var/www/html/wp-settings.php',
-  match => 'require_once\( ABSPATH \. WPINC \. \'/class-wp-locale\.phpp\' \);',
-  line  => 'require_once( ABSPATH . WPINC . \'/class-wp-locale.php\' );',
+exec { 'sed -i "s/.phpp/.php/g" /var/www/html/wp-settings.php':
+  path => '/usr/bin:/usr/sbin:/bin',
 }
 -> exec {'restart service':
   command => '/usr/sbin/service apache2 restart',
